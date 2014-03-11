eaac19c70e8ed288b966685c329eb4f898af5b7f
Jacob Apkon <japkon12@gmail.com>
2014-03-10 21:03:56 -0400

    Trying to render the map

--- transit/transit.js
+++ transit/transit.js
@@ -18,11 +18,11 @@ function initialize(){
 function locate_me(){
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
-			// console.log("hello");
+			console.log("hello");
 			lat = position.coords.latitude;
-			// console.log(lat);
+			console.log(lat);
 			lng = position.coords.longitude;
-			// console.log(lng);
+			console.log(lng);
 			render()
 		});
 	} else {
