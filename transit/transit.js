6cd147f0699aebf69aa49a2e36dacdf4cbcd6b30
Jacob Apkon <japkon12@gmail.com>
2014-03-10 22:04:44 -0400

    Added a 'your location' marker

--- transit/transit.js
+++ transit/transit.js
@@ -22,6 +22,7 @@ function initialize(){
 function locate_me(){
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
+			// Get my location
 			lat = position.coords.latitude;
 			lng = position.coords.longitude;
 			render()
@@ -33,7 +34,18 @@ function locate_me(){
 
 function render(){
 	me = new google.maps.LatLng(lat, lng);
+	// Move the map to my location
 	map.panTo(me);
+
+	marker = new google.maps.Marker({
+		position: me,
+		title: "You are here."
+	});
+
+	google.maps.event.addListener(marker, "click", function() {
+		infowindow.setContent(marker.title);
+		infowindow.open(map, marker);
+	});
 }
 
 function data_ready(){
