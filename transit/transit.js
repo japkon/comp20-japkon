7f4a99ef04fd696ff073a5e51adecfcffe44e7b2
Jacob Apkon <japkon12@gmail.com>
2014-03-13 01:53:29 -0400

    Testing finding closest stop

--- transit/transit.js
+++ transit/transit.js
@@ -80,7 +80,7 @@ var stops = [{"line":"blue", "name":"Bowdoin", "lat":42.361365, "lng":-71.062037
 
 function initialize(){
 	map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-	locate_me();
+	render();
 	xhr = new XMLHttpRequest();
 	xhr.open("get", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true); 
 	xhr.onreadystatechange = data_ready;
@@ -93,7 +93,6 @@ function locate_me(){
 			// Get my location
 			lat = position.coords.latitude;
 			lng = position.coords.longitude;
-			render()
 		});
 	} else {
 		alert("Location services are not supported by your browser.")
@@ -101,6 +100,7 @@ function locate_me(){
 }
 
 function render(){
+	locate_me();
 	me = new google.maps.LatLng(lat, lng);
 
 	marker = new google.maps.Marker({
